045849def97b5cce05c87ae8a9337c4530d747c2
xiaoyangxiaoen <xiaoyangxiaoen@diligentone.cn>
2025-04-29 15:37:36 +0800

    初始二段跳

--- src/views/components/moonlight/index.vue
+++ src/views/components/moonlight/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="content" ref="content">
   
-      <div class="layer1"></div>
+      <!-- <div class="layer1"></div>
       <div class="layer2"></div>
       <div class="layer3"></div>
       <div class="layer4"></div>
       <div class="layer5"></div>
-      <div class="z-title"></div>
+      <div class="z-title"></div> -->
   
       <div class="role"  :style="{
             left:role.x * unitX + 'px',
@@ -31,7 +31,7 @@ import { KEY } from '@/constants/key.ts';
 import cloneDeep from 'lodash/cloneDeep';
 
 // 帧数
-const FPS = ref(240)
+const FPS = ref(120)
 
 // 整个屏幕实例
 const content:any = ref(null)
@@ -52,6 +52,7 @@ const role = reactive<any>({
     w: 1, // 宽度 1 格
     h: 2, // 高度 2 格
     jumpStatus: false, // 是否跳跃
+    jumpSecondStatus: false, // 是否 二段 跳跃
     transition: 0.1, // 移动过渡效果
    // 匀速运动（如进度条）	linear  自然动作（如按钮悬停）	ease 或 ease-in-out  物体减速（如刹车）	ease-out  离散动画（如幻灯片切换）	steps(n) 特殊弹性效果	cubic-bezier()
     method: 'ease', // 过渡效果  ease
@@ -62,12 +63,15 @@ const role = reactive<any>({
     jumpHeight: 3, // 跳跃高度 格
     jumpYSpeed: 12, // 跳跃时候上下方向速度 
     jumpXSpeed: 0, // 跳跃时候左右方向速度 默认是 0 跳跃的时候按 左 右 键会左右移动
-    jumpXSpeedValue: 5, // 跳跃时候左右方向每次移动距离  和 jumpYSpeed 等效果
+    jumpXSpeedValue: 5, // 跳跃时候左右方向每次移动距离  和 jumpYSpeed 数值类似
     jumpYDirection: 'top', // 跳跃时候上下方向  top / down
     jumpXDirection: 'right', // 跳跃时候左右方向  left / right
 
 
 })
+
+// 存储上次角色信息
+const initialRole:any = ref(null);
 
 // 按下
 const handleKeyDown = (event:any) => {
@@ -76,8 +80,13 @@ const handleKeyDown = (event:any) => {
     let originRole = cloneDeep(role)
     if(KEY['UP'].includes(event.key)){
         if(!role.jumpStatus) {
+            initialRole.value = cloneDeep(role);
             role.jumpYDirection = 'top';
             !role.moveStatus && jump(originRole);
+        }else if(!role.jumpSecondStatus && initialRole.value){
+            role.jumpYDirection = 'top';
+            role.jumpHeight = role.y + role.jumpHeight;
+            !role.moveStatus && jumpSecond(cloneDeep(initialRole.value));
         }
     }else if(KEY['DOWN'].includes(event.key)){
         if(role.jumpStatus) {
@@ -137,12 +146,31 @@ const jump = (originRole?:any) => {
     },role.jumpTime * 1000 / FPS.value)
 }
 
+// 二段跳跃 最高距离是 role.y + role.jumpYSpeed
+const jumpSecond = (originRole?:any) => {
+    clearInterval(role.timer);  // 清除上一次的定时器 一段跳跃
+    role.jumpStatus = true;
+    role.jumpSecondStatus = true;
+    role.timer = setInterval(() => {
+        if(Math.abs(role.y - originRole.y) >= role.jumpHeight) {
+            role.jumpHeight = originRole.jumpHeight;
+            clearInterval(role.timer);
+            down(originRole);
+            return
+        }
+        // 同时修改 left 和 top 会触发 一次连贯动画  浏览器优化策略
+        role.jumpYDirection === 'top' ? role.y += role.jumpYSpeed / FPS.value : role.y -= role.jumpYSpeed / FPS.value;
+        role.jumpXDirection === 'right' ? role.x += role.jumpXSpeed / FPS.value : role.x -= role.jumpXSpeed / FPS.value;
+    },role.jumpTime * 1000 / FPS.value)
+}
+
 // 下落
 const down = (originRole?:any) => {
     role.jumpYDirection = 'down';
     role.timer = setInterval(() => {
         if(role.y <= originRole.y) {
             role.jumpStatus = false;
+            role.jumpSecondStatus = false;
             clearInterval(role.timer)
             return
         }
@@ -183,7 +211,7 @@ onUnmounted(() => {
       display: flex;
       justify-content: center;
       align-items: center;
-      background-color: #0883d8;
+    //   background-color: #0883d8;
       position: relative;
       .role{
         position: absolute;
