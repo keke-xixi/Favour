0cd073f74c01bbe24cc16ed240b439a97c6e46d0
tiantian2 <tiantian@9919>
2026-04-10 10:41:01 +0800

    fix

--- src/views/knowledge/components/work/2.vue
+++ src/views/knowledge/components/work/2.vue
@@ -20,6 +20,23 @@
         });
 
         npm run dev -- --port 3001 --host 127.0.0.1  指定端口启动项目
+
+
+        // 系统更新通知
+        import { webUpdateNotice } from '@plugin-web-update-notification/vite'
+
+        webUpdateNotice({
+            versionType: 'build_timestamp',
+            notificationConfig: {
+                placement: 'topLeft',
+            },
+            notificationProps: {
+                title: '📢 系统更新',
+                description: '系统更新啦，请刷新页面！',
+                buttonText: '刷新',
+                dismissButtonText: '忽略',
+            },
+        }),
   `)
 
    //将属性或者函数暴露给父组件
